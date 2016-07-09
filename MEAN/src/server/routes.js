090806cc7d26ed5e2ec464d528e357d9afe34229
arizona2014 <andy.lisac@gmail.com>
2016-07-09 21:38:30 +0200

    added mongoose support

--- MEAN/src/server/routes.js
+++ MEAN/src/server/routes.js
@@ -2,6 +2,29 @@ var router = require('express').Router();
 var four0four = require('./utils/404')();
 var data = require('./data');
 
+
+// Mongoose import
+var mongoose = require('mongoose');
+
+// Mongoose connection to MongoDB (ted/ted is readonly)
+mongoose.connect('mongodb://localhost/interview', function (error) {
+  if (error) {
+    console.log(error);
+  }
+});
+
+
+// Mongoose Schema definition
+var Schema = mongoose.Schema;
+var QuestionSchema = new Schema({
+  id: Number,
+  question: String,
+  answer: String
+});
+
+// Mongoose Model definition
+var Question = mongoose.model('questions', QuestionSchema);
+
 router.get('/random', getRandom);
 router.get('/people', getLastPeople);
 router.get('/categories', getCategories);
@@ -11,7 +34,10 @@ router.get('/*', four0four.notFoundMiddleware);
 module.exports = router;
 
 function getRandom(req, res, next) {
-  res.status(200).send(data.random);
+  //res.status(200).send(data.random);
+  Question.find({}, function (err, docs) {
+    res.json(docs);
+  });
 }
 
 function getLastPeople(req, res, next) {
